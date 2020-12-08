85055fca6f9ba7f922d601d43fe3f3a3dfded5c9
fiojiojfdlakfj <aidandubel@icloud.com>
2020-12-08 15:19:48 -0700

    kotlin builds 😊

--- TechnicalTerrorsCode/src/main/java/org/firstinspires/ftc/technicalterrorscode/TestKotlinOpmode.kt
+++ TechnicalTerrorsCode/src/main/java/org/firstinspires/ftc/technicalterrorscode/TestKotlinOpmode.kt
@@ -4,14 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction
 import com.qualcomm.robotcore.util.ElapsedTime
 import com.qualcomm.robotcore.util.Range
 
 @TeleOp(name = "Basic: Kotlin OpMode", group = "Technical Terrors")
-class TestIterativeOpmode : OpMode() {
+class TestKotlinOpmode : OpMode() {
     private val runtime: ElapsedTime = ElapsedTime()
     private var leftDrive: DcMotor? = null
     private var rightDrive: DcMotor? = null
+
+    var leftPower: Double = 0.00
+    var rightPower: Double = 0.00
 
     override fun init() {
         telemetry.addData("Status", "Initialized")
@@ -19,8 +23,8 @@ class TestIterativeOpmode : OpMode() {
         leftDrive = hardwareMap.get(DcMotor::class.java, "left_drive")
         rightDrive = hardwareMap.get(DcMotor::class.java, "right_drive")
 
-        leftDrive.setDirection(DcMotor.Direction.FORWARD)
-        rightDrive.setDirection(DcMotor.Direction.REVERSE)
+        leftDrive!!.setDirection(Direction.FORWARD)
+        rightDrive!!.setDirection(Direction.REVERSE)
 
         telemetry.addData("Status", "Initialized")
     }
@@ -32,16 +36,13 @@ class TestIterativeOpmode : OpMode() {
     }
 
     override fun loop() {
-        val leftPower: Double
-        val rightPower: Double
+        leftPower  = (-gamepad1.left_stick_y).toDouble()
+        rightPower = (-gamepad1.right_stick_y).toDouble()
 
-        leftPower  = -gamepad1.left_stick_y;
-        rightPower = -gamepad1.right_stick_y;
+        leftDrive!!.setPower(leftPower)
+        rightDrive!!.setPower(rightPower)
 
-        leftDrive.setPower(leftPower)
-        rightDrive.setPower(rightPower)
-
-        telemetry.addData("Status", "Run Time: " + runtime.toString())
+        telemetry.addData("Status", "Run Time: $runtime")
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower)
     }
 
